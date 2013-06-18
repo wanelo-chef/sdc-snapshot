3e4e08d4271a71ee86d6a299bed37acf2153cd3d
Blake Irvin <pair+blake@wanelo.com>
2013-06-18 12:46:30 -0700

    Make cookbook more data-driven.

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['sdc']['account_name'] = "my_account"
+default['sdc']['api_endpoint'] = "https://api.joyentcloud.com"
+default['sdc']['install_path'] = "/opt/local/bin"
+default['sdc']['snapshot_retention_count'] = "10"
