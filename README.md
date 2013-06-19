5c9111462984e7b43d8c5f4a766c008cf4cc87bd
Blake Irvin <pair+blake@wanelo.com>
2013-06-18 19:29:40 -0700

    Updates and improvements.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ sdc-snapshot
 =============
 
 Installs cli tools and scheduler for automated snapshots via Joyent's SDC API.
+
+For the tool to work, you must install an SSH keypair that you have uploaded through Joyent's public cloud portal into /root/.ssh
--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
-default['sdc']['account_name'] = "my_account"
+default['sdc']['account_name'] = "wanelo"
+default['sdc']['key_name'] = "sdc-snapshot"
 default['sdc']['api_endpoint'] = "https://api.joyentcloud.com"
 default['sdc']['install_path'] = "/opt/local/bin"
-default['sdc']['snapshot_retention_count'] = "10"
+default['sdc']['snapshot_retention_count'] = 10
