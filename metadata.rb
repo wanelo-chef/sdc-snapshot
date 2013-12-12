2f41147fee92f2b1ecb8407f9c618552464afdef
Eric Saxby & Konstantin Gredeskoul <pair+sax+kig@wanelo.com>
2013-12-11 16:47:45 -0800

    Run every hour instead of every 15 minutes

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name             'sdc-snapshot'
 maintainer       'Wanelo, Inc.'
-maintainer_email 'bixu@wanelo.com'
+maintainer_email 'ops@wanelo.com'
 license          'Apache 2.0'
 description      'Installs/Configures Joyent SDC CLI API tools for managing snapshots.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.2.2'
 
 depends "sdc"
--- recipes/default.rb
+++ recipes/default.rb
@@ -30,7 +30,7 @@ template "#{node['sdc']['install_path']}/sdc-listsnapshots" do
 end
 
 cron "snapshot the machine every 15 minutes and deletes older snapshots" do
-  minute "0,15,30,45"
+  minute "10"
   command "#{node['sdc']['install_path']}/sdc-snapshot 2>&1 >> /var/log/sdc-snapshot.log"
   only_if "ls /root/.ssh/#{node['sdc']['key_name']}"
 end
